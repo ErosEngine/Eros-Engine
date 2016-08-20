9d78dbefe46967026dc8cb3df0b56ac683094a29
Alex Kiecker <kiecker223@gmail.com>
2016-08-19 23:32:42 -0500

    Added some dependencies

---
+++ ui_OpeningDialog.h
@@ -0,0 +1,63 @@
+/********************************************************************************
+** Form generated from reading UI file 'OpeningDialog.ui'
+**
+** Created by: Qt User Interface Compiler version 5.7.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_OPENINGDIALOG_H
+#define UI_OPENINGDIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QHeaderView>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_OpeningDialog
+{
+public:
+    QDialogButtonBox *buttonBox;
+    QComboBox *comboBox;
+
+    void setupUi(QDialog *OpeningDialog)
+    {
+        if (OpeningDialog->objectName().isEmpty())
+            OpeningDialog->setObjectName(QStringLiteral("OpeningDialog"));
+        OpeningDialog->resize(316, 151);
+        buttonBox = new QDialogButtonBox(OpeningDialog);
+        buttonBox->setObjectName(QStringLiteral("buttonBox"));
+        buttonBox->setGeometry(QRect(-50, 100, 341, 32));
+        buttonBox->setOrientation(Qt::Horizontal);
+        buttonBox->setStandardButtons(QDialogButtonBox::Cancel|QDialogButtonBox::Ok);
+        comboBox = new QComboBox(OpeningDialog);
+        comboBox->setObjectName(QStringLiteral("comboBox"));
+        comboBox->setGeometry(QRect(20, 40, 261, 22));
+
+        retranslateUi(OpeningDialog);
+        QObject::connect(buttonBox, SIGNAL(accepted()), OpeningDialog, SLOT(accept()));
+        QObject::connect(buttonBox, SIGNAL(rejected()), OpeningDialog, SLOT(reject()));
+
+        QMetaObject::connectSlotsByName(OpeningDialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *OpeningDialog)
+    {
+        OpeningDialog->setWindowTitle(QApplication::translate("OpeningDialog", "Dialog", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class OpeningDialog: public Ui_OpeningDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_OPENINGDIALOG_H
